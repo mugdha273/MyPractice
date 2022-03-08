f13033b7840bbee324a46669265c170a01ac39df
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-03-08 21:46:02 +0530

    Time: 121 ms (21.95%), Space: 72.9 MB (7.66%) - LeetHub

---
+++ 134-gas-station/134-gas-station.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        vector<int>f;
+        
+        for(int i=0; i<gas.size(); i++)
+        {
+            f.push_back(gas[i]-cost[i]);
+        }
+        
+        int sum=0;
+        
+        for(auto x: f)sum+=x;
+        
+        int ans=0;
+        
+        if(sum>=0)
+        {
+            int totalcost = 0;
+        int currcost = 0;
+        int startIndex = 0;
+        
+        for(int i =0; i<gas.size(); i++){
+            
+
+            totalcost = totalcost + (gas[i] - cost[i]);
+            currcost = currcost + (gas[i] - cost[i]);
+            
+
+            if(currcost < 0){
+                currcost = 0;
+                startIndex = i +1;
+            }
+        }
+        
+
+        if(totalcost >= 0)
+        return startIndex;
+            
+        }
+        else
+        {
+            return -1;
+        }
+        
+        return ans;
+    }
+};
