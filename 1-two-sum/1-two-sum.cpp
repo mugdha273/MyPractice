5121390f9054dc15d2fb7354b38148c45cdc08b0
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-04-06 16:36:37 +0530

    Time: 8 ms (95.38%), Space: 11 MB (28.97%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
+    
+    static bool Compare_(const pair<int,int> &a ,const  pair<int,int> &b){
+        return a.second < b.second;
+        
+    }
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        vector<pair<int,int>>s;
         
-        vector<pair<int,int>>arr;
-        for(int i=0; i<n;i++) arr.push_back(make_pair(nums[i],i)); 
-        
-        sort(arr.begin(), arr.end()); 
-        int l = 0, r = n- 1;
-        
-        while(l<r)
+        for(int i=0; i<nums.size(); i++)
         {
-            int sum = arr[l].first + arr[r].first;
-            cout<<l<<" "<<r<<" "<<sum<<endl;
-            
-            if (sum==target) return { arr[l].second,arr[r].second};
-            if(sum>target)
-            {
-                r=r-1;
-            }
-            else l=l+1;
-            
+            s.push_back({i, nums[i]});
         }
-        return {};
+        
+        sort(s.begin(), s.end(), Compare_);
+        vector<int>vec;
+        int i=0;
+        int j = s.size()-1;
+        
+        while(i<j)
+        {
+            if( s[i].second + s[j].second == target ) 
+            { 
+                vec.push_back(s[i].first);
+                vec.push_back(s[j].first);
+                return vec;
+            }
+            if(s[i].second + s[j].second > target)
+            {
+                j--;
+            } 
+            else
+            {
+                i++;
+            }
+        }
+        
+        return vec;
     }
 };
