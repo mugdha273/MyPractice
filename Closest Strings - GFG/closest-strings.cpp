3332fc41211dabd0aaac952d566440cdb4b2ef84
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-04-06 13:07:32 +0530

    Added solution - LeetHub

---
+++ Closest Strings - GFG/closest-strings.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int shortestDistance(vector<string> &s, string word1, string word2)
+	{
+		// Your code goes here
+		
+		int a = -1, b = -1;
+		int ans = INT_MAX;
+		
+		for(int i=0 ; i<s.size(); i++)
+		{
+		    if(s[i] == word1)
+		    {
+		        a = i;
+		    }
+		    
+		    if(s[i] == word2)
+		    {
+		        b = i;
+		    }
+		    
+		  //  cout<<a<<" "<<b<<" "<<ans<<endl;
+		  if(a!= -1 && b!= -1)
+		    ans = min(abs(a-b), ans);
+		}
+		
+		return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		int n;
+   		cin >> n;
+
+   		vector<string> s(n);
+   		for(int i = 0; i < n; i++)
+   			cin >> s[i];
+
+   		string word1, word2;
+
+   		cin >> word1 >> word2;
+   		
+   		Solution ob;
+
+   		cout << ob.shortestDistance(s, word1, word2) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
