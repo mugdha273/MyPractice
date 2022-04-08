b9d8b35526c9c4e5231528670f613ba1792b1b97
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-04-08 13:47:43 +0530

    Time: 64 ms (33.03%), Space: 89.3 MB (64.06%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    
-    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>res;
+        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
+        int n1 = nums1.size();
+        int n2 = nums2.size(); 
+        int low = 0, high = n1;
         
-        int x= nums1.size()-1, y= nums2.size()-1;
-        int i=0,j=0;
-        while(i<= x && j<= y)
-        {
-            if(nums1[i]<nums2[j])
-            {
-                res.push_back(nums1[i++]);
+        while(low <= high) {
+            int cut1 = (low+high) >> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1; 
+            
+        
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1]; 
+            
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2]; 
+            
+            
+            if(left1 <= right2 && left2 <= right1) {
+                if( (n1 + n2) % 2 == 0 ) 
+                    return (max(left1, left2) + min(right1, right2)) / 2.0; 
+                else 
+                    return max(left1, left2); 
             }
-            else res.push_back(nums2[j++]);
+            else if(left1 > right2) {
+                high = cut1 - 1; 
+            }
+            else {
+                low = cut1 + 1; 
+            }
         }
-        while(i<=x)res.push_back(nums1[i++]);
-        while(j<=y)res.push_back(nums2[j++]);
-        
-        int s = res.size();
-        double ans;
-//         for(auto x: res)cout<<x<<" ";
-//         cout<<"\n";
-        
-//         cout<<s<<endl;
-        if(s%2 == 0)
-        {
-            ans = (res[(s/2)-1] + res[s/2])*0.5;
-            
-            return ans;
-            
-        }
-        else
-        {
-            ans = res[s/2];
-            return ans;
-        }
+        return 0.0; 
         
     }
 };
