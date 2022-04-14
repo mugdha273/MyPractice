de4df621743db3a426bec4eb1adf900785ed295a
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-04-14 17:12:49 +0530

    Time: 239 ms (19.53%), Space: 96.2 MB (12.87%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,52 +1,37 @@
-//Merge Sort Easy code
-
 class Solution {
 public:
-    
-    void mergeSort(vector<int>& nums, int l, int r)
+    void mergeSort(vector<int>&nums, int l, int r)
     {
-        if(l<r)
-        {
-            int mid = l + (r-l)/2;
-            mergeSort(nums, l, mid);
-            mergeSort(nums,mid+1, r);
+        if(l<r){
             
-            merge(nums, l, mid, r);
+        int mid = l + (r-l)/2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid+1, r);
+        merge(nums,l,mid,r);
+            
         }
     }
     
-    void merge(vector<int>&nums, int l, int mid, int r)
+    void merge(vector<int>&nums, int l,int mid, int r)
     {
-        int n1 = mid-l+1;
-        int n2= r - mid;
+        int n1= mid-l+1;
+        int n2= r-mid;
         
         vector<int>v1(n1);
         vector<int>v2(n2);
         
-        for(int i=0; i<n1; i++)
-        {
-            v1[i]= nums[l+i];
-        }
+        for(int i=0; i<n1; i++) v1[i]= nums[l+i];
+        for(int i=0; i<n2; i++) v2[i]= nums[mid+1+i];
         
-        for(int i=0; i<n2; i++)
-        {
-            v2[i] = nums[mid+1+i];
-        }
-        
-        int i=0, j=0, k=l;
+        int i = 0; 
+        int j = 0;
+        int k=l;
         
         while(i<n1 && j<n2)
         {
-            if(v1[i]<= v2[j])
-            {
-                nums[k++]= v1[i++];
-            }
-            else
-            {
-                nums[k++] = v2[j++];
-            }
+            if(v1[i]<=v2[j]) nums[k++] = v1[i++];
+            else nums[k++] = v2[j++];
         }
-        
         while(i<n1)
         {
             nums[k++]= v1[i++];
@@ -61,5 +46,6 @@ public:
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums, 0, nums.size()-1);
         return nums;
+        
     }
 };
