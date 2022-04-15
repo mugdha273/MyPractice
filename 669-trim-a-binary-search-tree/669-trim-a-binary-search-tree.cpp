a0c33ede4c09149bf2264f1b23a84ec7bc472fa9
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-04-15 06:45:20 +0530

    Time: 24 ms (43.03%), Space: 23.9 MB (74.78%) - LeetHub

---
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimleft(TreeNode* root, int low)
+    {
+        if(root == nullptr) return NULL;
+        if(root->val<low) return trimleft(root->right, low);
+        else root->left=trimleft(root->left, low);
+        return root;
+    }
+    TreeNode* trimright(TreeNode* root, int high)
+    {
+        if(root == nullptr) return NULL;
+        if(root->val>high) return trimright(root->left, high);
+        else root->right= trimright(root->right, high);
+        return root;
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        root = trimleft(root, low);
+        root = trimright(root, high);
+        return root;
+        
+    }
+};
