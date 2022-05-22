820bdb1b1c24d362ba39c18f60c9c4003098bde1
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-05-22 18:41:30 +0530

    Time: 3 ms (96.90%), Space: 9 MB (30.32%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
-        vector<int> ans;
+        
+        int n= nums2.size();
+        stack<int>st;
+        vector<int>ans;
         unordered_map<int, int> umap;
         
-       for(int i = nums2.size() - 1; i >= 0; i--) {
+        for(int i=n-1; i>=0; i--)
+        {
             
-            while(!st.empty() && st.top() < nums2[i]){
-                st.pop();
+            if(st.size()==0)
+            {
+                 umap[nums2[i]] = -1;
             }
-            
-            if(st.empty()) {
-                umap[nums2[i]] = -1;
-            } else {
-                umap[nums2[i]] = st.top();
+            else if(st.size()>0 && st.top()>nums2[i])
+            {
+                 umap[nums2[i]] = st.top();
             }
-            st.push(nums2[i]);   
+            else if (st.size()>0 && st.top()<= nums2[i] )
+            {
+                while(st.size()>0 && st.top()<=nums2[i])
+                {
+                    st.pop();
+                }
+                
+                if(st.size()==0)
+                {
+                    umap[nums2[i]] = -1;
+                }
+                else
+                {
+                    umap[nums2[i]] = st.top();
+                }
+            }
+            st.push(nums2[i]);
         }
         
-        for(int i = 0; i < nums1.size(); i++) {
+         for(int i = 0; i < nums1.size(); i++) {
             ans.push_back(umap[nums1[i]]);
         }
         
-        return ans;
         
+        return ans;
     }
 };
