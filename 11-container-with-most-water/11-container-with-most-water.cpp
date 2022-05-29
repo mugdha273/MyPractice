05a3533009664e5d6c4990f89761e4dcf393ffef
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-05-29 10:39:20 +0530

    Time: 132 ms (39.10%), Space: 58.9 MB (82.07%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,15 +4,16 @@ public:
         int l = 0;
         int r = height.size()-1;
         int maxarea = 0;
-        while(l<r)
+        while(l<=r)
         {
             int area= (r-l)*(min(height[r], height[l]));
             maxarea = max(maxarea, area);
             
-            if(height[l] < height[r]) ++l;
-            else --r;
+            if(height[l]<height[r]) l++;
+            else r--;
         }
         
         return maxarea;
+        
     }
 };
