1a2ec503d60673b4e62233c6e829a6b716514d9a
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-06-06 18:24:07 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (49.07%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     
-    set<vector<int>>ans;
+    vector<vector<int>>ans;
     
     void helper(vector<int>&nums, int i, vector<int>&sub)
     {
         if(i==nums.size())
         {
-            ans.insert(sub);
+            ans.push_back(sub);
             return;
         }
         
-        helper(nums,i+1,sub);
+        //when you choose to select ith element
         sub.push_back(nums[i]);
-        helper(nums,i+1,sub); 
+        helper(nums,i+1,sub);
+        
+        
+        //when you are skipping the element make sure you skipped the duplicate as well
+        while(i+1<nums.size() && nums[i]==nums[i+1]) i++;
+        
         sub.pop_back();
+        helper(nums,i+1,sub); 
+        
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<int>sub;
         helper(nums,0,sub);
-        vector<vector<int>>res;
-        for(auto x:ans) res.push_back(x);
         
-        return res;
+        return ans;
     }
 };
