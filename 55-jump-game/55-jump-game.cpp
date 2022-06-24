2935d97ad2c0b6a355345681f1749141a2f0a6cf
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-06-24 19:16:48 +0530

    Time: 133 ms (28.31%), Space: 48.5 MB (42.65%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         if(nums.size()==1) return true;
-        int boundary = nums[0];
         int n= nums.size()-1;
+        int boundary=nums[0];
         
-        for(int i=0; i<=boundary ; i++)
+        for(int i=0; i<=boundary; i++)
         {
-            if(i+nums[i]>=boundary)
-            {
-                boundary= i+nums[i];
-            }
+            if(boundary<= i+nums[i]) boundary= i+nums[i];
             if(boundary>=n) return true;
+            // cout<<boundary<<" ";
         }
         
         return false;
