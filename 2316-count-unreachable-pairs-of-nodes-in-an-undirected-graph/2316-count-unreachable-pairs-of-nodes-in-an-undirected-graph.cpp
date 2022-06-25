2cd93352ed7933b7b377e2aae4332ca11a30def1
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-06-25 21:37:57 +0530

    Time: 728 ms (50.00%), Space: 185.7 MB (50.00%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    typedef long long ll;
+     void dfs(int node, vector<vector<int>>& adj, ll& cnt, vector<int>& vis){
+        vis[node] = 1;
+        cnt++;
+        for(auto& i: adj[node]){
+            if(vis[i]==0) dfs(i,adj,cnt,vis);   
+        }
+    }
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(n);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        ll ans = ((ll)n*(n-1))/2;
+        
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){ // as node is not visited, we find the no. of nodes in current component.
+                ll cnt = 0;
+                dfs(i,adj,cnt,vis);
+                ans -= (cnt*(cnt-1))/2;
+            }
+        }
+        return ans;
+        
+    }
+};
