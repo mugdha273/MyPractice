d1127549d5580cdaf6e5fa59935c0f0a766e0393
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-07-01 16:42:13 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -9,29 +9,69 @@ using namespace std;
 
 class Solution 
 {
+    private:
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if(M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
     public:
     //Function to find if there is a celebrity in the party or not.
-    
-    
-    int celebrity(vector<vector<int> >& m, int n) 
+    int celebrity(vector<vector<int> >& M, int n) 
     {
-        // code here 
-        vector<int>in(n,0);
-        vector<int>out(n,0);
-        for(int i=0; i<m.size(); i++)
-        {
-            for(int j=0; j<m[i].size(); j++)
+        stack<int> s;
+        //step1: push all element in stack
+        for(int i=0; i<n; i++) {
+            s.push(i);
+        }   
+        
+        //step2: get 2 elements and copare them
+        
+        while(s.size() > 1) {
+            
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if(knows(M,a,b,n)){
+                s.push(b);
+            }
+            else
             {
-                in[j]+= m[i][j];
-                out[i]+=m[i][j];
+                s.push(a);
             }
         }
+        int ans = s.top();
+        //step3: single element in stack is potential celeb
+        //so verify it
         
-        for(int i=0; i<n; i++)
-        if(in[i] == n-1 && out[i] == 0)
-            return i;
+        int zeroCount = 0;
         
-        return -1;
+        for(int i=0; i<n; i++) {
+            if(M[ans][i] == 0)
+                zeroCount++;
+        }
+        
+        //all zeroes
+        if(zeroCount != n)
+            return -1;
+        
+        //column check
+        int oneCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[i][ans] == 1)
+                oneCount++;
+        }
+        
+        if(oneCount != n-1)
+            return -1;
+        
+        return ans;
+        
     }
 };
 
