2919c366893bee64c4d71411f6e74c8322a82c19
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-07-05 17:39:52 +0530

    Time: 15 ms (18.79%), Space: 8.7 MB (48.15%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int dp[201][201];
-    int rec(vector<vector<int>>& t, int i, int j)
-    {
-        if(i==t.size()) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        return dp[i][j]=t[i][j]+min(rec(t,i+1,j), rec(t,i+1,j+1));
-    }
     
+    int helper(int i, int j, vector<vector<int>>&triangle)
+    {
+        if(i>= triangle.size()||j>=triangle[i].size()) return 0;
+        if(i==triangle[i].size()) return triangle[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=min(triangle[i][j]+helper(i+1,j,triangle), triangle[i][j]+helper(i+1,j+1,triangle));
+        
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         memset(dp,-1,sizeof(dp));
-        return rec(triangle,0,0);
+        return helper(0,0,triangle);
     }
 };
