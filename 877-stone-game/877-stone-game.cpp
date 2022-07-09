39ce80f994ad87d89d88462a7078fb0729664f4f
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-07-09 19:19:06 +0530

    Time: 41 ms (22.30%), Space: 12 MB (33.51%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    // bool stoneGame(vector<int>& piles) {
+    //     return true;
+    // }
+    
+    int dp[1005][1005];
+    
+    int f(int st, int end, vector <int> &piles) {
+        if(st == end-1) return max(piles[st], piles[end]);
+        
+        if(dp[st][end] != -1) return dp[st][end];
+        
+        int ifleft = piles[st] + max(f(st+2, end, piles), f(st+1, end-1, piles));
+        int ifright = piles[end] + max(f(st+1, end-1, piles), f(st, end-2, piles));
+        
+        return dp[st][end] = max(ifleft, ifright);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int l = 0, r = piles.size()-1;
+        memset(dp, -1, sizeof(dp));
+        int alex = f(l, r, piles);
+        int sum = 0;
+        for(int i=0;i<piles.size();i++) sum += piles[i];
+
+        return alex > (sum - alex);
     }
 };
