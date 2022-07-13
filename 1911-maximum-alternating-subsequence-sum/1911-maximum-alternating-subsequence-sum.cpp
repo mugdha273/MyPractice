f6d419084275ad6b8fdb9985932a3a233bff4519
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-07-13 21:02:38 +0530

    Time: 302 ms (52.71%), Space: 102.3 MB (44.47%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     //Mahhhn! I'm so proud! I wrote the recursive solution all by myself in one go.
     long long dp[100000][2];
-    vector<int> a;
-    int n;
-    long long helper(int i, int state)
+    long long helper(vector<int>&nums, int i, int state)
     {
-        if(i>=n)
+        if(i>=nums.size())
         {
             return 0;
         }
-        long long ans=INT_MIN;
+        long long a=INT_MIN;
         if(dp[i][state]!=-1) return dp[i][state];
-
-        if(state==0){
-            ans=max(helper(i+1,1)+a[i],helper(i+1,0));
+        if(state==0)
+        {
+          long long take= helper(nums,i+1,1)+nums[i];
+          long long notTake=helper(nums,i+1,0)+0;
+          a= max(take,notTake);
         }
-        else if(state==1){
-            ans=max(helper(i+1,0)-a[i],helper(i+1,1));
+        else if(state==1)
+        {
+          long long take= helper(nums,i+1,0)-nums[i];
+          long long notTake=helper(nums,i+1,1)-0;
+          a= max(take,notTake);
         }
-        return dp[i][state]=ans;
+        return dp[i][state]=a;
     }
     long long maxAlternatingSum(vector<int>& nums) {
-        a=nums;
-        n=nums.size();
         memset(dp,-1,sizeof(dp));
-        return helper(0,0);
+        return helper(nums,0,0);
     }
 };
