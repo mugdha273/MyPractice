88e78c717a99aea916b315f7cc7de2d11b99f9a4
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2022-07-15 21:08:24 +0530

    Time: 482 ms (19.43%), Space: 135.3 MB (7.60%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,26 +1,26 @@
-//right, its common sense bruh!
-
 class Solution {
 public:
+    void helper(vector <int> v, int index, int &ans){
+	if(index==v.size()){
+		ans++;
+		return;
+	}
+	for(int i=index;i<v.size();i++){
+		if(v[i]%(index+1)==0 || (index+1)%v[i]==0){
+			swap(v[i], v[index]);
+			helper(v, index+1, ans);
+			swap(v[i], v[index]);
+		}
+	}
+}
     
-    int res = 0;
-
-    void permute(vector<int> &arr, int idx) {
-        if (idx == arr.size()) res++;
-        
-        for (int i = idx; i < arr.size(); i++) {
-            swap(arr[i], arr[idx]);
-            if (arr[idx]%(idx+1) == 0 || (idx+1)%arr[idx] == 0)
-                permute(arr, idx+1);
-            swap(arr[i], arr[idx]);
-        }
-    }
-    int countArrangement(int n) {
-        vector<int> arr(n);
-        for (int i = 1; i <= n; i++) {
-            arr[i-1] = i;
-        }
-        permute(arr, 0);
-        return res;
-    }
+int countArrangement(int N) {
+	vector <int> v;
+	int ans=0;
+	for(int i=1; i <= N; i++){
+		v.push_back(i);
+	}
+	helper(v, 0, ans);
+	return ans;
+}
 };
